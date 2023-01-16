466934de577fc1dc64d037abfbbf0054726cd0ed
Sam Goddard <hello@samgoddard.co.uk>
2023-01-16 13:30:59 +0000

    new styles started

--- pages/journal/index.js
+++ pages/journal/index.js
@@ -72,9 +72,8 @@ export default function Journal(initialData) {
           exit="exit"
         >
           <m.article>
-            {/* Fixed Sidebar */}
-            <div className="fixed top-0 right-0 bottom-0 w-[29.75vw] h-screen pt-[45px] md:pt-[53px] xl:pt-[57px] col-span-3 col-start-8 border-l border-black px-5 hidden md:flex flex-wrap">
-              <div className="w-full mt-auto py-5">
+            <div className="absolute top-0 right-0 bottom-0 w-[25%] h-screen pt-[45px] md:pt-[53px] xl:pt-[57px] col-span-3 col-start-8 hidden lg:flex flex-wrap">
+              {/* <div className="w-full mt-auto py-5">
                 { current !== null && (
                   <div className="w-full relative overflow-hidden">
                     { journals[current].images.length == 1 ? (
@@ -90,36 +89,111 @@ export default function Journal(initialData) {
                     )}
                   </div>
                 )}
-              </div>
+              </div> */}
+
+              {journals.slice(1).map((e, i) => {
+                return (
+                  <Link href={`/journal/${e.slug.current}`} key={i}>
+                    <a onMouseEnter={()=> updateCurrent(i)} onMouseLeave={()=> removeCurrent()} className={`p-5 border-b border-black w-full block ${current == i || !isHovering ? 'opacity-100' : 'opacity-30' }`}>
+                      <div className="mb-3 w-full">
+                      { e.images.length == 1 ? (
+                        <Image
+                          image={e.images[0]}
+                          focalPoint={e.images[0].asset.hotspot}
+                          layout="responsive"
+                          sizes="(min-width: 768px) 80vw, 100vw"
+                          className="w-full"
+                        />
+                      ) : (
+                        <Gif images={e.images} />
+                      )}
+                      </div>
+                      <h2 className="text-lg md:text-lg xl:text-xl uppercase leading-none md:leading-none xl:leading-none block mb-2 md:mb-2 xl:mb-2">{e.title}</h2>
+                      <div className="flex pb-1 mb-5">
+                        <span className="font-serif mb-2 block text-lg leading-none">( SW.0{i + 1} )</span>
+                      </div>
+
+                      <span className="inline-block text-base md:text-base xl:text-base leading-[1.5] md:leading-[1.5] xl:leading-[1.5] a11y-focus relative mx-auto">
+                        Read
+                        <span className="absolute bottom-0 left-0 right-0 w-full h-[1px] bg-black"></span>
+                      </span>
+                    </a>
+                  </Link>
+                )
+              })}
             </div>
 
-            {/* Main Section */}
             <Grid>
-              <div className="col-span-10 md:col-span-7 pt-32 md:pt-[20vh] xl:pt-[30vh] relative">
-                <svg className="mx-5 w-3 absolute top-0 left-0 mb-3 pt-[10vh] hidden md:block" viewBox="0 0 10 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.64 28c-.907-1.893-2.347-3.653-4.32-5.28v-1.4c.853.4 1.6.8 2.24 1.2.64.373 1.173.747 1.6 1.12V.32h1.68v23.32c.453-.373 1-.747 1.64-1.12.64-.4 1.373-.8 2.2-1.2v1.4C7.733 24.347 6.293 26.107 5.36 28h-.72Z" fill="#242B2D"/></svg>
-                
-                {journals.map((e, i) => {
-                  return (
-                    <Link href={`/journal/${e.slug.current}`} key={i}>
-                      <a onMouseEnter={()=> updateCurrent(i)} onMouseLeave={()=> removeCurrent()} className={`p-5 pb-[0.3vw] md:pb-[0.6vw] xl:pb-[0.8vw] block ${current == i || !isHovering ? 'opacity-100' : 'opacity-30' }`}>
-                        <div className="flex pb-1">
-                          <span className="font-serif mb-2 block text-lg leading-none">( SW.0{i + 1} )</span>
-                          {/* <span className="font-serif mb-2 block text-base leading-none ml-auto text-right">( {e.date} )</span> */}
+              <div className="col-span-12 w-full pt-12 lg:pt-0 lg:w-[75%] lg:fixed top-0 left-0 right-0 lg:h-screen flex flex-wrap lg:border-r lg:border-black">
+
+                <div className="lg:my-auto w-full lg:flex lg:items-center lg:justify-center">
+                  <div className="lg:w-[55vh] lg:mt-[-6vh]">
+                    <Link href={`/journal/${journals[0].slug.current}`}>
+                      <a className={`p-5 pb-[0.3vw] md:pb-[0.6vw] xl:pb-[0.8vw] hidden lg:block`}>
+                        <div className="w-full mt-auto py-5">
+                          <div className="pb-1">
+                            <span className="font-serif mb-2 block text-lg leading-none text-center">( SW.01 )</span>
+                          </div>
+                          <div className="w-full relative overflow-hidden">
+                            { journals[0].images.length == 1 ? (
+                              <Image
+                                image={journals[0].images[0]}
+                                focalPoint={journals[0].images[0].asset.hotspot}
+                                layout="responsive"
+                                sizes="(min-width: 768px) 80vw, 100vw"
+                                className="w-full"
+                              />
+                            ) : (
+                              <Gif images={journals[0].images} />
+                            )}
+                          </div>
                         </div>
-                        <div className="border-t border-black pt-4">
-                          <h2 className="font-display text-[6.4vw] md:text-[3.35vw] xl:text-[3vw] leading-none mb-6 md:mb-8 max-w-[70%] md:max-w-[65%]">{e.title}</h2>
+                        <div className="flex justify-center">
+                          <h2 className="text-xl md:text-2xl 2xl:text-3xl uppercase leading-none md:leading-none 2xl:leading-none block mb-2 md:mb-3 xl:mb-4 text-center w-[90%]">{journals[0].title}</h2>
+                        </div>
+                        
+                        <div className="flex justify-center">
+                          <span className="inline-block text-lg md:text-lg xl:text-lg leading-[1.5] md:leading-[1.5] xl:leading-[1.5] a11y-focus relative mx-auto pt-[3vh]">
+                            Read Article
+                            <span className="absolute bottom-0 left-0 right-0 w-full h-[1px] bg-black"></span>
+                          </span>
                         </div>
                       </a>
                     </Link>
-                  )
-                })}
-              
-                <div className="col-span-10 md:col-span-7">
-                  <div className="mb-[13vw] md:mb-[8.5vw] mt-[15vw] px-5">
-                    <div className="w-full h-[1px] bg-black -skew-y-12"></div>
+                    
+                    <div className="lg:hidden mb-24">
+                      {journals.map((e, i) => {
+                        return (
+                          <Link href={`/journal/${e.slug.current}`} key={i}>
+                            <a onMouseEnter={()=> updateCurrent(i)} onMouseLeave={()=> removeCurrent()} className={`p-5 pb-[0.3vw] md:pb-[0.6vw] xl:pb-[0.8vw] block ${current == i || !isHovering ? 'opacity-100' : 'opacity-30' }`}>
+                              <div className="mb-3">
+                              { e.images.length == 1 ? (
+                                <Image
+                                  image={e.images[0]}
+                                  focalPoint={e.images[0].asset.hotspot}
+                                  layout="responsive"
+                                  sizes="(min-width: 768px) 80vw, 100vw"
+                                  className="w-full"
+                                />
+                              ) : (
+                                <Gif images={e.images} />
+                              )}
+                              </div>
+                              <h2 className="text-lg md:text-lg xl:text-xl uppercase leading-none md:leading-none xl:leading-none block mb-2 md:mb-3 xl:mb-4 w-[80%]">{e.title}</h2>
+                              <div className="flex pb-1">
+                                <span className="font-serif mb-2 block text-lg leading-none">( SW.0{i + 1} )</span>
+                              </div>
+                            </a>
+                          </Link>
+                        )
+                      })}
+                    </div>
                   </div>
                 </div>
-                <Footer noRightPad />
+
+                <div className="w-full lg:mt-auto lg:absolute lg:bottom-0 lg:left-0 lg:right-0">
+                  <Footer noRightPad />
+                </div>
               </div>
             </Grid>
           </m.article>
